30e103b168290705f109137f8e68021f39774bb8
Belgast-TSG <timothysgibbons@gmail.com>
2024-09-11 09:43:22 -0400

    adjusted count_characters function to return a list of discitonaries

--- main.py
+++ main.py
@@ -3,9 +3,9 @@ def main():
     text = get_book_text(book_path)
     words = word_count(text)
     characters = count_characters(text)
-    print(text)
-    print(f"{words} in document")
+    characters.sort(key=sort_on, reverse=True)
     print(characters)
+    
 
 def get_book_text(path):
     with open(path) as f:
@@ -19,10 +19,13 @@ def count_characters(text):
     lowered_text = text.lower()
     text_dict = {}
     for c in lowered_text:
-        if c in text_dict:
-            text_dict[c] += 1
-        else:
-            text_dict[c] = 1
-    return text_dict
+        if c.isalpha():
+            if c in text_dict:
+                text_dict[c] += 1
+            else:
+                text_dict[c] = 1
+    return [{"character": char, "count": count} for char, count in text_dict.items()]
 
+def sort_on(dict):
+    return dict["count"]
 main()
